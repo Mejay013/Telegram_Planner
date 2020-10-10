48a8e1c15656bd059b9aa9a86e942c63db9c5b8f
Mejay <arseniy.kocharov012@gmail.com>
2020-10-10 20:35:20 +0300

    add main google api

---
+++ Event.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
+class Event():
+    def __init__(self):
+        self.SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+    def сreate_creds(self):
+        creds = None
+
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+                
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                creds = flow.run_local_server(port=0)
+
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        return(creds)
+
+    def normal_date(self,date :str):
+        bad_date = date.split('.')
+        good_date = f'{bad_date[-1]}-{bad_date[0]}'
+        return(good_date)
+
+    def create_event(self,date,task_name, time_start, time_end):
+        creds = self.сreate_creds()
+        
+        service = build('calendar', 'v3', credentials=creds)
+        event = {
+            'summary': task_name,
+            'start': {
+                'dateTime': f'2020-{self.normal_date(date)}T09:00:00-{time_start}',
+                'timeZone': 'Europe/Moscow',
+            },
+            'end': {
+                'dateTime': f'2020-{self.normal_date(date)}T09:00:00-{time_end}',
+                'timeZone': 'Europe/Moscow',
+            },
+            }
+
+        event = service.events().insert(calendarId='arseniy.kocharov012@gmail.com', body=event).execute()
+
+
+if __name__ == '__main__':
+    a = Event()
+    a.create_event('13.10', 'Some event', '2020-10-12T09:00:00-12:30', '2020-10-12T09:00:00-13:30')
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import vk_api
-from vk_api.longpoll import VkLongPoll,VkEventType
 from datetime import datetime
 import telebot
 from telebot import types
 import stock
+from Event import Event
 
 
 bot = telebot.TeleBot(stock.token)
-
 keyboard = types.InlineKeyboardMarkup()
+
+event = None
 
 @bot.message_handler(commands=['start']) # вступителная функция
 def start_message(message):
@@ -18,13 +18,25 @@ def start_message(message):
 
 @bot.callback_query_handler(func=lambda call: True) #обработка кнопок
 def callback_worker(call):
-    if call.data == "new_event": 
-        bot.send_message(call.message.chat.id, 'Новое действие')
+    global event
+    if call.data == "new_event":
+        event = 'new_event'
+        bot.send_message(call.message.chat.id, 'Введите событие \n Пример 13.10/Новая задача/17:40/18:40')
+        
+
+def create_event(date,task_name,time_end,time_start):
+    event = Event()
+    event.create_event(date,task_name,time_end,time_start)
+    print()
+
 
 @bot.message_handler(content_types=['text']) #обработка текстовых сообщений
 def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    print(event)
+    if event != None:
+        if event == 'new_event':
+            date,task_name,time_start,time_end = message.text.split('/')
+            print(date,task_name,time_end,time_start)
 
 
 
