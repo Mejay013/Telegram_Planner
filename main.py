1e524acece67f1f688f2e5d855f5f104cada10f0
Mejay <arseniy.kocharov012@gmail.com>
2020-10-11 17:35:17 +0300

    finally

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from Event import Event
 
 bot = telebot.TeleBot(stock.token)
 keyboard = types.InlineKeyboardMarkup()
-
 event = None
 
 def send_message(user_id, text, check_keyboard = False):
@@ -20,6 +19,7 @@ def send_message(user_id, text, check_keyboard = False):
 
 @bot.message_handler(commands=['start']) # вступителная функция
 def start_message(message):
+    set_keyboard()
     send_message(message.from_user.id, 'Выберите действие', True) 
 
 @bot.callback_query_handler(func=lambda call: True) #обработка кнопок
@@ -27,17 +27,21 @@ def callback_worker(call):
     global event
     if call.data == "new_event":
         event = 'new_event'
-        bot.send_message(call.message.chat.id, 'Введите событие \n Пример 13.10/Новая задача/17:40/18:40')
-        
+        send_message(call.message.chat.id, 'Введите событие \n Пример 13.10/Новая задача/17:40/18:40')
     
 def create_events(date,task_name,time_start,time_end):
     event = Event()
     event.create_event(date,task_name,time_start,time_end)
 
+def set_keyboard():
+    global keyboard
+    key_event = types.InlineKeyboardButton(text='Добавить событие', callback_data='new_event')
+    keyboard.add(key_event)
+
 
 @bot.message_handler(content_types=['text']) #обработка текстовых сообщений
 def get_text_messages(message):
-    if event != None:
+    if event != None and message.from_user.id == stock.my_id:
         if event == 'new_event':
             date,task_name,time_start,time_end = message.text.split('/')
             create_events(date,task_name,time_start,time_end)
